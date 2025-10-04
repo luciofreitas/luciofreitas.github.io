73f05bb2444d391ec337f9b204ffbdf776e2922a
luciofreitas <luciodfp@gmail.com>
2025-10-04 14:23:02 -0300

    chore: save work

--- src/pages/page-SejaPro.jsx
+++ src/pages/page-SejaPro.jsx
@@ -19,11 +19,12 @@ export default function SejaPro() {
           <div className="seja-pro-cards">
             <div className="seja-pro-card">
               <h3 className="seja-pro-card-title">Básico</h3>
-              <p className="seja-pro-card-lead">Todos os benefícios da versão Básica.</p>
+              <p className="seja-pro-card-lead">Benefícios da versão Básica.</p>
               <div className="seja-pro-card-body">
                 <ul className="seja-pro-features">
                   <li>Acesso ao buscador de peças.</li>
                   <li className="seja-pro-feature-unavailable">Acesso ao endereço das lojas que vendem as peças.</li>
+                  <li className="seja-pro-feature-unavailable">Acesso ao valores da Tabela FIPE.</li>
                   <li>Suporte via email.</li>
                   <li className="seja-pro-feature-unavailable">Suporte via whatsapp.</li>
                   <li className="seja-pro-feature-unavailable">Comunidade no discord.</li>
@@ -33,11 +34,12 @@ export default function SejaPro() {
 
             <div className="seja-pro-card seja-pro-card-pro">
               <h3 className="seja-pro-card-title">Pro</h3>
-              <p className="seja-pro-card-lead">Todos os benefícios da versão Pro.</p>
+              <p className="seja-pro-card-lead">Benefícios da versão Pro.</p>
               <div className="seja-pro-card-body">
                 <ul className="seja-pro-features">
                   <li> Acesso ao buscador de peças.</li>
                   <li> Acesso ao endereço das lojas que vendem as peças.</li>
+                  <li> Acesso ao valores da Tabela FIPE.</li>
                   <li> Suporte via email.</li>
                   <li> Suporte via whatsapp.</li>
                   <li> Comunidade no discord.</li>
