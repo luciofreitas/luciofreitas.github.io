dd820db3a54277a587cf8c1c6194b136d5b6adb2
luciofreitas <luciodfp@gmail.com>
2025-10-16 19:19:17 -0300

    chore: move logo.png into assets and update references

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -21,7 +21,7 @@ const Logo = ({ className = '', onClick, ...props }) => {
         {...props}
       >
           <img 
-            src="/logo.png" 
+            src="./assets/logo.png" 
             alt="Logo Peça Fácil" 
             className="logo-image"
           />
